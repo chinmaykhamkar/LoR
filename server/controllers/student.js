ba2660b5bc236da06c544aebfe397762b3976758
chinmaykhamkar <khamkarchinmay4@gmail.com>
2021-07-28 23:52:49 +0530

    added controller

---
+++ server/controllers/student.js
@@ -0,0 +1,5 @@
+const getStudent = (req,res)=>{
+    res.send('route is working');
+};
+
+module.exports = getStudent;
--- server/routes/student.js
+++ server/routes/student.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const getStudent = require('../controllers/student');
 
-router.get('/',(req,res)=>{
-    res.send('route is working');
-});
+    router.get('/',getStudent);
 
-module.exports = router;
+module.exports = router; 
