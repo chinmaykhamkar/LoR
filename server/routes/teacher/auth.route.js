65ab37eca459dc0e538b2ad28e39588bf13ca0ee
chinmaykhamkar <khamkarchinmay4@gmail.com>
2021-08-20 19:45:15 +0530

    major changes

--- server/routes/teacher/auth.route.js
+++ server/routes/teacher/auth.route.js
@@ -10,11 +10,8 @@ const {
 } = require("../../controllers/teacher/auth.controller");
 
 router.route("/register").post(register);
-
 router.route("/login").post(login);
-
 router.route("/forgotpassword").post(forgotPassword);
-
 router.route("/passwordreset/:resetToken").put(resetPassword);
 
 module.exports = router;
